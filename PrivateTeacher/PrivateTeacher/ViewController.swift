1782a3bb139076966312e7b7ef4422e6a4427dda
AfafAlqahtani <Afaf.Alqahtani.jiz.s@tuwaiq.edu.sa>
2021-12-24 09:07:40 +0300

    init xcode  project structure

---
+++ PrivateTeacher/PrivateTeacher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrivateTeacher
+//
+//  Created by Afaf Yahya on 20/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
